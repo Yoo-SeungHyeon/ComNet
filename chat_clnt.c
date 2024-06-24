f2215f881945ab37361725ecb04a5f3d6bc3fe00
Yoo SeungHyeon <effort-result@naver.com>
2024-06-24 11:15:58 +0900

    주제 1 완료

--- chat_clnt.c
+++ chat_clnt.c
@@ -6,81 +6,85 @@
 #include <sys/socket.h>
 #include <pthread.h>
 	
-#define BUF_SIZE 100
-#define NAME_SIZE 20
-	
+#define BUF_SIZE 100 //최대 버퍼 크기
+#define NAME_SIZE 20 //최대 이름 크기
+
+//함수 호출
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
-	
+
+//전역변수 선언
 char name[NAME_SIZE]="[DEFAULT]";
 char msg[BUF_SIZE];
 	
 int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in serv_addr;
-	pthread_t snd_thread, rcv_thread;
+	int sock; //서버와 연결할 소켓 변수 선언
+	struct sockaddr_in serv_addr; //서버 정보 구조체 선언 (IP, Port 등등 선언)
+	pthread_t snd_thread, rcv_thread; //Thread 타입 변수 선언
 	void * thread_return;
+
+	// 클라이언트 실행시 IP와 Port, Name을 입력했는지 확인.
 	if(argc!=4) {
 		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
 		exit(1);
 	 }
 	
-	sprintf(name, "[%s]", argv[3]);
-	sock=socket(PF_INET, SOCK_STREAM, 0);
+	sprintf(name, "[%s]", argv[3]); //입력받은 Name을 이용해 전송할 메세지 앞에 넣을 별명 생성
+	sock=socket(PF_INET, SOCK_STREAM, 0); //PF_INET = IPv4, SOCK_STREAM + 0 = TCP
 	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
+	memset(&serv_addr, 0, sizeof(serv_addr)); //구조체의 모든 값 0으로 선언
+	serv_addr.sin_family=AF_INET; //IP 종류 선언. IPv4
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]); //IP지정
+	serv_addr.sin_port=htons(atoi(argv[2])); //Port 지정
 	  
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) //연결 에러 발생시 실행
 		error_handling("connect() error");
 	
-	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
-	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
-	pthread_join(snd_thread, &thread_return);
-	pthread_join(rcv_thread, &thread_return);
-	close(sock);  
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock); //전송 쓰레드 생성
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock); //수신 쓰레드 생성
+	pthread_join(snd_thread, &thread_return); //전송 쓰레드 종료되지 않게 Block
+	pthread_join(rcv_thread, &thread_return); //수신 쓰레드 종료되지 않게 Block
+	close(sock);  //소켓 종료
 	return 0;
 }
 	
 void * send_msg(void * arg)   // send thread main
 {
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
-	while(1) 
+	int sock=*((int*)arg); //소켓값으로 변수 초기화
+	char name_msg[NAME_SIZE+BUF_SIZE]; //메세지 변수 선언
+	while(1)
 	{
-		fgets(msg, BUF_SIZE, stdin);
-		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
+		fgets(msg, BUF_SIZE, stdin); //메세지를 msg에 저장
+		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) //q나 Q입력시 종료
 		{
 			close(sock);
 			exit(0);
 		}
-		sprintf(name_msg,"%s %s", name, msg);
-		write(sock, name_msg, strlen(name_msg));
+		sprintf(name_msg,"%s %s", name, msg); //name과 msg를 합쳐서 전송할 name_msg 생성
+		write(sock, name_msg, strlen(name_msg)); //write를 통해 전송
 	}
 	return NULL;
 }
 	
 void * recv_msg(void * arg)   // read thread main
 {
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
-	int str_len;
+	int sock=*((int*)arg); //소켓 파일 디스크립터 초기화
+	char name_msg[NAME_SIZE+BUF_SIZE]; //메세지를 저장할 변수 선언
+	int str_len; //길이 변수 선언
 	while(1)
 	{
-		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
-		if(str_len==-1) 
+		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1); //name_msg에 값을 저장
+		if(str_len==-1)  // -1 반환시 종료
 			return (void*)-1;
-		name_msg[str_len]=0;
-		fputs(name_msg, stdout);
+		name_msg[str_len]=0; // 배열 제일 뒤에 0 추가. 출력할때 끝이라고 알려주는 역할
+		fputs(name_msg, stdout); //화면에 출력
 	}
 	return NULL;
 }
 	
-void error_handling(char *msg)
+void error_handling(char *msg) //에러 발생시 에러 메세지를 통해 처리
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
--- chat_serv.c
+++ chat_serv.c
@@ -7,73 +7,80 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
-#define BUF_SIZE 100
-#define MAX_CLNT 256
+#define BUF_SIZE 100 //최대 버퍼 크기
+#define MAX_CLNT 256 //최대 클라이언트 수
 
+//함수 선언
 void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
 
+//전역변수 선언
 int clnt_cnt=0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_adr, clnt_adr;
-	int clnt_adr_sz;
-	pthread_t t_id;
+	int serv_sock, clnt_sock; //서버 소켓과 클라이언트 소켓 선언
+	struct sockaddr_in serv_adr, clnt_adr; //주소 구조체 선언
+	int clnt_adr_sz; //클라이언트 주소 크기 변수 선언
+	pthread_t t_id; //쓰레드 변수 선언
+	
+	// 프로그래 실행시 Port지정했는지 확인
 	if(argc!=2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-  
-	pthread_mutex_init(&mutx, NULL);
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET; 
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
 	
-	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+	pthread_mutex_init(&mutx, NULL); //데이터 일관성을 위해 mutex 선언
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0); //소켓 생성. PF_INET = IPv4, SOCK_STREAM + 0 = TCP
+
+	memset(&serv_adr, 0, sizeof(serv_adr)); //주소 구조체 전부 0으로 초기화
+	serv_adr.sin_family=AF_INET;  //타입 선언. IPv4
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY); //주소 선언. 자기자신
+	serv_adr.sin_port=htons(atoi(argv[1])); //포트 선언. 프로그램 실행때 입력한 값
+	
+
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1) //bind후, 실패시 에러 메세지실행
 		error_handling("bind() error");
-	if(listen(serv_sock, 5)==-1)
+
+	if(listen(serv_sock, 5)==-1) //listen 실행 및 실패시 에러 메세지 출력
 		error_handling("listen() error");
 	
 	while(1)
 	{
-		clnt_adr_sz=sizeof(clnt_adr);
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+		clnt_adr_sz=sizeof(clnt_adr); //클라이언트 주소 크기 저장
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz); //클라이언트 소켓 생성
 		
-		pthread_mutex_lock(&mutx);
-		clnt_socks[clnt_cnt++]=clnt_sock;
-		pthread_mutex_unlock(&mutx);
+		pthread_mutex_lock(&mutx); //mutex lock을 통해 변수 선점
+		clnt_socks[clnt_cnt++]=clnt_sock; //클라이언트 배열에 클라이언트 소켓 저장
+		pthread_mutex_unlock(&mutx); //mutex unlock을 통해 변수 접근 허용
 	
-		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
-		pthread_detach(t_id);
-		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock); //클라이언트 별 쓰레드 및 handle_clnt 실행
+		pthread_detach(t_id); //클라이언트 쓰레드 종료 후 자동 으로 자원 반환
+		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr)); //연결한 클라이언트의 IP 출력
 	}
-	close(serv_sock);
+	close(serv_sock); //서버 소켓 종료
 	return 0;
 }
 	
 void * handle_clnt(void * arg)
 {
-	int clnt_sock=*((int*)arg);
-	int str_len=0, i;
-	char msg[BUF_SIZE];
+	int clnt_sock=*((int*)arg); //클라이언트 소켓 정보 초기화
+	int str_len=0, i; //길이 변수 선언
+	char msg[BUF_SIZE]; //메세지 배열 선언
 	
-	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0)
-		send_msg(msg, str_len);
+	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0) //큐에 메세지가 존재한다면
+		send_msg(msg, str_len); //모든 클라이언트에게 메세지 전송
 	
-	pthread_mutex_lock(&mutx);
+	pthread_mutex_lock(&mutx); //데이터 선점
 	for(i=0; i<clnt_cnt; i++)   // remove disconnected client
-	{
-		if(clnt_sock==clnt_socks[i])
+	{								//앞선 while문을 빠져 나왔다는건 소켓을 종료한다는 의미 = clnt_socks배열에서 빠져야 한다는 의미
+		if(clnt_sock==clnt_socks[i])  //배열에 존재한다면
 		{
-			while(i <clnt_cnt-1)
+			while(i <clnt_cnt-1) //해당 배열을 제외하고 한칸씩 당김
 			{
 				clnt_socks[i]=clnt_socks[i+1];
 				  i++;
@@ -84,19 +91,19 @@ void * handle_clnt(void * arg)
 		}
 	}
 	clnt_cnt--;
-	pthread_mutex_unlock(&mutx);
-	close(clnt_sock);
+	pthread_mutex_unlock(&mutx); //데이터 접근 허용
+	close(clnt_sock); //클라이언트 소켓 종료.
 	return NULL;
 }
 void send_msg(char * msg, int len)   // send to all
 {
 	int i;
-	pthread_mutex_lock(&mutx);
+	pthread_mutex_lock(&mutx); //데이터 선점
 	for(i=0; i<clnt_cnt; i++)
-		write(clnt_socks[i], msg, len);
-	pthread_mutex_unlock(&mutx);
+		write(clnt_socks[i], msg, len); //클라이언트 배열을 통해 모든 클라이언트에게 전송
+	pthread_mutex_unlock(&mutx); //데이터 접근 허용
 }
-void error_handling(char * msg)
+void error_handling(char * msg) //에러 메세지 출력 함수
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
